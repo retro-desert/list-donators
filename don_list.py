a3987ff36e24805abefc87dc79d568c82a407bc4
retro-desert <48768258+retro-desert@users.noreply.github.com>
2021-01-09 16:59:31 +0000

    Add files via upload

---
+++ don_list.py
@@ -0,0 +1,55 @@
+from web3 import Web3
+import json
+
+infura_url = "https://rinkeby.infura.io/" # Input your own API
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+contract_address = "0x4127d3839458Abe58C5387E9D19869d198fbe840" # Input your own address
+contract_abi = [
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [],
+		"name": "allDonatorsList",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "list",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+print("Donators list:")
+s = contract.functions.allDonatorsList().call().split(" ")
+for i in s:
+	if i == "":
+		s.remove(i)
+		continue
+	s[s.index(i)] = Web3.toChecksumAddress(str(i))
+print(s)
